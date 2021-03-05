1762a45ab1da9a54aa9047c5bef6abe3ca6930c3
kseniia-grishchenko <kseniarctybz@gmail.com>
2021-03-05 14:35:47 +0200

    Add frontend: screens, components, actions, reducers

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Container} from "react-bootstrap";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import HomeScreen from "./screens/homeScreen";
+import ProductScreen from "./screens/productScreen";
+import CartScreen from "./screens/cartScreen";
+import LoginScreen from "./screens/loginScreen";
+import RegisterScreen from "./screens/registerScreen";
+import ProfileScreen from "./screens/profileScreen";
+import UserListScreen from "./screens/userListScreen";
+import UserEditScreen from "./screens/userEditScreen";
+import ShippingScreen from "./screens/shippingScreen";
+import PaymentScreen from "./screens/paymentScreen";
+import PlaceOrderScreen from "./screens/placeOrderScreen";
+import OrderScreen from "./screens/orderScreen";
+
+function App() {
+  return (
+    <Router>
+        <Header/>
+        <main className="py-3">
+            <Container>
+                <Route path="/" component={HomeScreen} exact/>
+                <Route path="/login" component={LoginScreen}/>
+                <Route path="/register" component={RegisterScreen}/>
+                <Route path="/profile" component={ProfileScreen}/>
+                <Route path="/shipping" component={ShippingScreen}/>
+                <Route path="/payment" component={PaymentScreen}/>
+                <Route path="/placeorder" component={PlaceOrderScreen}/>
+                <Route path="/order/:id" component={OrderScreen}/>
+                <Route path="/product/:id" component={ProductScreen}/>
+                <Route path="/cart/:id?" component={CartScreen}/>
+                <Route path="/admin/userlist" component={UserListScreen}/>
+                <Route path="/admin/user/:id/edit" component={UserEditScreen}/>
+            </Container>
+        </main>
+        <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/userEditScreen.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/loader";
+import Message from "../components/message";
+import FormContainer from "../components/formContainer";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
+import {whenMapDispatchToPropsIsObject} from "react-redux/lib/connect/mapDispatchToProps";
+
+function UserEditScreen({match, history}){
+    const userId = match.params.id
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [is_admin, setIs_Admin] = useState(false)
+
+    const dispatch = useDispatch()
+
+    const userDetails = useSelector(state => state.userDetails)
+    const { error, loading, user } = userDetails
+
+    const userUpdate = useSelector(state => state.userUpdate)
+    const { error: errorUpdate, loading: loadingUpdate, success: successUpdate } = userUpdate
+
+    useEffect(() => {
+        if(successUpdate){
+            dispatch({type: USER_UPDATE_RESET})
+            history.push('/admin/userlist')
+        } else {
+            if(!user.name || user._id !== Number(userId)){
+                dispatch(getUserDetails(userId))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+                setIs_Admin(user.is_admin)
+            }
+        }
+    }, [user, userId, successUpdate, history])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(updateUser({_id: user._id, name, email, is_admin}))
+    }
+
+    return (
+        <div>
+
+            <Link to='/admin/userlist'>
+                Go Back
+            </Link>
+            <FormContainer>
+                <h1>Edit User</h1>
+
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+
+                {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> :
+                    (
+                        <Form onSubmit={submitHandler}>
+
+                            <Form.Group controlId='name'>
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    type='name'
+                                    placeholder='Enter Name'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId='email'>
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control
+                                    type='email'
+                                    placeholder='Enter Email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+
+                            <Form.Group controlId='isadmin'>
+                                <Form.Check
+                                    type='checkbox'
+                                    checked={is_admin}
+                                    label='Is Admin'
+                                    onChange={(e) => setIs_Admin(e.target.checked)}
+                                >
+                                </Form.Check>
+                            </Form.Group>
+
+                            <Button type='submit' variant='primary'>
+                                Update
+                            </Button>
+                        </Form>
+                )}
+
+            </FormContainer>
+        </div>
+    )
+
+
+}
+
+export default UserEditScreen
